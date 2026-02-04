49c1c31300f7c404361f5dbdf93c9d9349f0985d
Arunkumar-Embedded <arunkumarpalani18@gmail.com>
2026-02-04 23:00:27 +0530

    variables

--- Add.c
+++ Add.c
@@ -1,12 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int a = printf("enter the value of a: ");
-    scanf("%d",&a);
+int main() {
+    int a, b;
+    int add, sub, mul, mod;
+    float div;
 
-    int b = printf("enter the value of b: ");
-    scanf("%d",&b);
+    // Getting value from user
+    printf("enter the number of a : ");
+    scanf("%d", &a); // & symbol address-ah kurikkum
+    printf("enter the number of b : ");
+    scanf("%d", &b);
 
-    int c = a + b;
-    printf("The sum of %d\n", c);
+    // Operations
+    add = a + b;
+    sub = a - b;
+    mul = a * b;
+    div = (float)a / b; 
+    mod = a % b;
+
+    // Results
+    printf("\n--- Result Vandhruchu! ---\n");
+    printf("Addition       : %d\n", add);
+    printf("Subtraction    : %d\n", sub);
+    printf("Multiplication : %d\n", mul);
+    printf("Division       : %.2f\n", div);
+    printf("Remainder (%)  : %d\n", mod);
+
+    return 0;
 }
